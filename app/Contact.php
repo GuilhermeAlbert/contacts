9e06cfbfc192e686e7e0d2e6fa66b42e0a533e1a
Guilherme Albert <guilhermealbert14@gmail.com>
2020-05-21 20:24:29 -0300

    Add laravel passport

---
+++ app/Contact.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Passport\HasApiTokens;
+
+class Contact extends Model
+{
+    use HasApiTokens, SoftDeletes;
+
+    /**
+     * Instancing table of database
+     *
+     * @var string
+     */
+    protected $table = 'contacts';
+
+    /**
+     * The primary key of table
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name',
+        'email',
+        'phone',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        // 
+    ];
+
+    /**
+     * Instancing dates of the table
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
