be77b65e505b37aa16c93868234dd344fcd66d8b
Guilherme Albert <guilhermealbert14@gmail.com>
2020-05-22 18:55:29 -0300

    Passport adjustments

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,11 @@ use Illuminate\Support\Facades\Route;
  */
 Route::group(['middleware' => ['auth:api']], function () {
     Route::resource('users', 'UserController');
+    Route::resource('contacts', 'ContactController');
 });
 
 /**
  * Routes without middleware
  */
 Route::group([], function () {
-    Route::resource('contacts', 'ContactController');
 });
